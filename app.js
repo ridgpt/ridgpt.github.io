38bbc51b7e93913c72ef75e24b326adb02107ce5
amin <amin@contact.im>
2024-10-05 09:58:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -142,7 +142,7 @@ document.getElementById('checkBtn').addEventListener('click', async function() {
 
         loader.style.display = 'none';
         resultElement.innerHTML = `
-<button id="retrainBtn" style="margin-top: 0px; margin-bottom: 20px; font-size: 1em;">
+<button id="retrainBtn" style="margin-top: 0px; margin-bottom: 20px; font-size: 0.9em;">
     Refine Content Checker <i class="fa-solid fa-arrows-rotate"></i>
     <h6 class="small">Only attempt to refine the content checker if you believe that the given results are 100% inaccurate. <i class="fa-solid fa-circle-info"></i></h6>
 </button>
