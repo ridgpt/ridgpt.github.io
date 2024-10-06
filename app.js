db68f5bb8710904b1e93f5cd1c698eabcf11431c
amin <amin@contact.im>
2024-10-06 16:10:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -146,17 +146,18 @@ document.getElementById('checkBtn').addEventListener('click', async function() {
     Refine Content Checker <i class="fa-solid fa-rotate-right"></i>
     <h6 class="small" style="margin-top:0px;">Only attempt to refine the content checker when delayed results and/or inaccurate results become frequent. <i class="fa-solid fa-circle-info"></i></h6>
 </button>
-            <h6 class="reshed" style="margin-bottom: 20px;">Involvement-Based Results <i class="fa-solid fa-chevron-down gradient-icon"></i></h6>
+            <h6 class="reshed" style="margin-bottom: 0px;">Results <i class="fa-solid fa-chevron-down gradient-icon"></i></h6>
+                        <h6 class="reshed" style="font-size:0.7rem; margin-top: 0px;">Never use these results to make decisions on someone's academic standing or career, as these results may not always be accurate. <i class="fa-solid fa-triangle-exclamation"></i></h6>
+
+                        <h6 class="reshed" style="font-size:0.7rem; margin-top: 20px;">The results below are involvement-based.  <i class="fa-solid fa-circle-info"></i></h6>
 
            <i class="fa-solid fa-chevron-right gradient-icon"></i> This content was ${analysisResult.text}
             <hr>
-            <h6 class="reshed" style="margin-bottom: 20px;">Summary-Based Results <i class="fa-solid fa-chevron-down gradient-icon"></i></h6>
+                        <h6 class="reshed" style="font-size:0.7rem; margin-top: 20px;">The results below are involvement-based.  <i class="fa-solid fa-circle-info"></i></h6>
             
-            <h6 style="opacity: 1;"><i class="fa-solid fa-chevron-right gradient-icon"></i> <span style="color: #FF0000;">AI</span> Involvement: ${analysisResult.aiPercentage}% <i class="fa-solid fa-robot"></i></h6>
+            <h6 style="opacity: 1; margin-top: 20px;"><i class="fa-solid fa-chevron-right gradient-icon"></i> <span style="color: #FF0000;">AI</span> Involvement: ${analysisResult.aiPercentage}% <i class="fa-solid fa-robot"></i></h6>
 
             <h6 style="margin-bottom: 0px; opacity: 1;"><i class="fa-solid fa-chevron-right gradient-icon"></i> <span style="color: #34C759; ">Human</span> Involvement: ${analysisResult.humanPercentage}% <i class="fa-solid fa-brain"></i></h6>
-            
-            <h6 class="reshed" style="font-size:0.7rem; margin-top: 20px;">Never use these results to make decisions on someone's academic standing or career, as these results may not always be accurate. <i class="fa-solid fa-triangle-exclamation"></i></h6>
         `;
 
         // Only display result element if analysisResult is valid
