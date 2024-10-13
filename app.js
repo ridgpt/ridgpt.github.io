1b19773128efe4236e6e80d65dd189de7360b305
amin <amin@contact.im>
2024-10-12 21:59:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ function createModal(modalFileName, deviceType) {
             if (appInstructions) {
                 if (deviceType === 'ios') {
                     appInstructions.innerHTML = `
+        <hr>
+        <h6 class="reshed" style="margin-bottom: 0px;">How To Get The App <i class="fa-solid fa-chevron-down gradient-icon"></i></h6>
                         <h6 class="reshed" style="font-size: 0.7rem;">For the best experience, it is recommended that you get the RidGPT iOS app. <i class="fa-solid fa-circle-info"></i></h6>
                         <h6 style="text-align: left;"><i class="fa-solid fa-chevron-right gradient-icon"></i> Click the "share" button. <i class="fa-solid fa-arrow-up-right-from-square"></i></h6>
                         <h6 style="text-align: left;"><i class="fa-solid fa-chevron-right gradient-icon"></i> Click "add to home screen". <i class="fa-regular fa-square-plus"></i></h6>
@@ -35,6 +37,8 @@ function createModal(modalFileName, deviceType) {
                     `;
                 } else if (deviceType === 'android') {
                     appInstructions.innerHTML = `
+        <hr>
+        <h6 class="reshed" style="margin-bottom: 0px;">How To Get The App <i class="fa-solid fa-chevron-down gradient-icon"></i></h6>
                         <h6 class="reshed" style="font-size: 0.7rem;">For the best experience, it is recommended that you get the RidGPT Android app. <i class="fa-solid fa-circle-info"></i></h6>
                         <h6 style="text-align: left;"><i class="fa-solid fa-chevron-right gradient-icon"></i> Click the "Download" button below. <i class="fa-solid fa-arrow-down"></i></h6>
                         <h6 style="text-align: left; margin-bottom: 0px;"><a href="/RidGPT.apk" class="button">Download Android App</a></h6>
